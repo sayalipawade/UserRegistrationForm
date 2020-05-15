aaf98c04991ae67b81fb2f4344de0a94a398e8a1
sayalipawade <sayalipawade05@gmail.com>
2020-05-15 08:38:54 +0530

    Added:Password should have minimum 8 characters

--- User.js
+++ User.js
@@ -5,9 +5,10 @@ console.log("***********Welcome to user registration**********")
 /**
  * Constants
  */
-let validNamePattern = new RegExp("^[A-Z]([a-zA-Z]{2,})$");
-let validEmailPattern=new RegExp("^[a-z][a-z0-9]*[.+-]?[a-z0-9]+[@]{1}[a-z0-9]+[.]{1}[a-z]{2,3}([.]{1}[a-z]{2,3})?$");
-let validMobilePattern=new RegExp("^[0-9]{2}[0-9]{10}");
+let VALIDNAMEPATTERN = new RegExp("^[A-Z]([a-zA-Z]{2,})$");
+let VALIDEMAILPATTERN=new RegExp("^[a-z][a-z0-9]*[.+-]?[a-z0-9]+[@]{1}[a-z0-9]+[.]{1}[a-z]{2,3}([.]{1}[a-z]{2,3})?$");
+let VALIDMOBILEPATTERN=new RegExp("^[0-9]{2}[0-9]{10}");
+let VALIDPASSWORD=new RegExp("^[a-zA-Z]{8,}$");
 
 /**
  * Function to matching the pattern
@@ -16,33 +17,43 @@ let validMobilePattern=new RegExp("^[0-9]{2}[0-9]{10}");
  */
 function patternMatching(inputPattern,validPattern) 
 {
-	if(validPattern.test(inputPattern))
-		console.log("Valid");
-	else
-		console.log("Invalid");
+    if(validPattern.test(inputPattern))
+    {
+        console.log("Valid");
+    }
+    else
+    {
+        console.log("Invalid");
+    }
 }
 
 /**
  * Checking first name is valid or not
  */
 let firstName = read.question("Enter valid first name : ");
-patternMatching(firstName,validNamePattern);
+patternMatching(firstName,VALIDNAMEPATTERN);
 
 /**
  * Checking last name is valid or not
  */
 let lastName=read.question("Enter valid Last name:");
-patternMatching(lastName,validNamePattern);
+patternMatching(lastName,VALIDNAMEPATTERN);
 
 /**
  * Checking email is valid or not
  */
 let email=read.question("Enter Email:");
-patternMatching(email,validEmailPattern);
+patternMatching(email,VALIDEMAILPATTERN);
 
 /**
  * Checking mobile is valid or not
  */
 let mobile=read.question("Enter Mobile:");
-patternMatching(mobile,validMobilePattern);
+patternMatching(mobile,VALIDMOBILEPATTERN);
+
+/**
+ * Checking password is minimum 8 characters or not
+ */
+let password=read.question("Enter Password:");
+patternMatching(password,VALIDPASSWORD);
 
