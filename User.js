3d2e996315cc6083fdb8ad00c1c21145404f1144
sayalipawade <sayalipawade05@gmail.com>
2020-05-15 07:52:43 +0530

    Added:Checking first name is valid or not

--- User.js
+++ User.js
@@ -1,1 +1,27 @@
+let read=require('readline-sync');
+
 console.log("***********Welcome to user registration**********")
+
+/**
+ * variables
+ */
+let validNamePattern = new RegExp("^[A-Z]([a-zA-Z]{2,})$");
+
+/**
+ * Function to matching the pattern
+ * @param {*} inputPattern 
+ * @param {*} validPattern 
+ */
+function patternMatching(inputPattern,validPattern) 
+{
+	if(validPattern.test(inputPattern))
+		console.log("Valid");
+	else
+		console.log("Invalid");
+}
+
+/**
+ * Checking first name is valid or not
+ */
+let firstName = read.question("Enter valid first name : ");
+patternMatching(firstName,validNamePattern);
